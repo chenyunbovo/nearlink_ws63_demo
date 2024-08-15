113350c9f095bb1e4651c19ea059942496d63f38
unknown <1669028482@qq.com>
2024-08-15 17:38:56 +0800

    新增MQTT5.0部分的代码示例

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 ## 示例介绍
 | 名称 | 描述 |
 | ----------- | ----------- |
-| mqtt_client_demo | 通过WIFI连接上路由器，并与自己搭建的EMQX服务器建立MQTT TCP连接 |
+| mqtt3_client_demo | 通过WIFI连接上路由器，并与自己搭建的EMQX服务器建立MQTT V3.1.1 TCP连接 |
+| mqtt5_client_demo | 通过WIFI连接上路由器，并与自己搭建的EMQX服务器建立MQTT V5.0 TCP连接 |
 | http_server_demo | 通过WIFI连接上路由器，绑定IP和端口，通过浏览器访问HTTP SERVER |
 | http_client_demo | 通过WIFI连接上路由器，通过HTTP GET百度网页数据 |
 | fix_sta_sample | 将原来的轮询代码修改为事件通知形式 |
--- mqtt5_client_demo/mqtt_client.c
+++ mqtt5_client_demo/mqtt_client.c
@@ -31,9 +31,9 @@ void publish(MQTTClient client, char *topic, char *payload)
     message.qos = QOS;
     message.retained = 0;
     MQTTClient_deliveryToken token;
-    MQTTClient_publishMessage(client, topic, &message, &token);
+    MQTTClient_publishMessage5(client, topic, &message, &token);
     MQTTClient_waitForCompletion(client, token, TIMEOUT);
-    printf("Send `%s` to topic `%s` \n", payload, TOPIC);
+    printf("Send `%s` to topic `%s` \n", payload, topic);
 }
 
 // Define the on_message callback function to print the content of the messages received by the subscribed topic
@@ -50,38 +50,63 @@ static int on_message(void *context, char *topicName, int topicLen, MQTTClient_m
 
 void mqtt_client_deinit(void)
 {
-    MQTTClient_disconnect(client, TIMEOUT);
+    enum MQTTReasonCodes reasonCode = 0;
+    MQTTProperties props;
+    MQTTClient_disconnect5(client, 0, reasonCode, &props);
     MQTTClient_destroy(&client);
 }
 
 void mqtt_client_init(void) 
 {
     int rc;
+    MQTTResponse ret;
     MQTTClient_init_options ops;
     ops.do_openssl_init = 0;
     MQTTClient_global_init(&ops);
-    rc = MQTTClient_create(&client, ADDRESS, CLIENTID, 0, NULL);
+    MQTTClient_createOptions create_opts = { {'M', 'Q', 'C', 'O'}, 0, MQTTVERSION_5 };
+
+    rc = MQTTClient_createWithOptions(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_DEFAULT, NULL, &create_opts);
     if (rc != MQTTCLIENT_SUCCESS) {
         printf("Failed to create mqtt client, return code %d\n", rc);
     }
     // 设置MQTT消息接受回调函数
     MQTTClient_setCallbacks(client, NULL, NULL, on_message, NULL);
-
-    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer5;
     conn_opts.username = USERNAME;
     conn_opts.password = PASSWORD;
+    MQTTProperties props = MQTTProperties_initializer;
+    MQTTProperties willProps = MQTTProperties_initializer;
 
+    
     #if USE_SSL
         MQTTClient_SSLOptions ssl_opts = configureSSLOptions();
         conn_opts.ssl = &ssl_opts;
     #endif
 
-    rc = MQTTClient_connect(client, &conn_opts);
-    if (rc != MQTTCLIENT_SUCCESS) {
-        printf("Failed to connect mqtt, return code %d\n", rc);
-    } else {
-        // subscribe topic
-        MQTTClient_subscribe(client, TOPIC, QOS);
+    ret = MQTTClient_connect5(client, &conn_opts, &props, &willProps);
+    if (ret.reasonCode == MQTTREASONCODE_SUCCESS) {
         printf("Connected to MQTT Broker!\n");
+    } else {
+        printf("failed to connect to MQTT Broker, return code %d\n", (int8_t)ret.reasonCode);
     }
+    MQTTResponse_free(ret);
+    // subscribe topic
+    MQTTSubscribe_options subopts = MQTTSubscribe_options_initializer;
+
+    subopts.retainAsPublished = 1;
+    subopts.noLocal = 0;
+    subopts.retainHandling = 0;
+
+    ret = MQTTClient_subscribe5(client, TOPIC, QOS, &subopts, &props);
+    if (ret.reasonCode == MQTTREASONCODE_SUCCESS) {
+        printf("subscribe topic succ!\n");
+    } else {
+        printf("failed to connect to MQTT Broker, return code %d\n", (int8_t)ret.reasonCode);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        publish(client,"test_pub","1223121313");
+        (void)osDelay(100);
+    }
+    MQTTResponse_free(ret);
 }
